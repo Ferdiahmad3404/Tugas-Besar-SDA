9b450868989dd0256da3bff000ca81ead12b9faf
FerdiAries <ferdiahmad3404@gmail.com>
2023-05-16 23:07:30 +0700

    Finish but with manual input and so many bugs

--- Bagan Pertandingan Master/mainPertandingan.c
+++ Bagan Pertandingan Master/mainPertandingan.c
@@ -1,12 +1,11 @@
 /*
 Dekripsi : main program bagan pertandingan
 Tanggal Pembuatan : 8/5/2023
+Update : 16/05/2023 23:07
 Author : Ferdi, Hanif, Revandi.
 */
 
 #include "automaticBinary.c"
-
-
 
 int main() {
     
